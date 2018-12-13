858011708d2272ad54634ef7f6e91743dc241919
Mark Pashmfouroush <dr.pashinator.m.d@gmail.com>
2018-12-13 16:36:59 +0000

    Initial TC support

--- netlink-packet/src/rtnl/message.rs
+++ netlink-packet/src/rtnl/message.rs
@@ -3,6 +3,7 @@ use failure::ResultExt;
 
 use crate::{
     AddressBuffer, AddressMessage, DecodeError, Emitable, LinkBuffer, LinkMessage, Parseable,
+    TcMessage,
 };
 
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -14,6 +15,15 @@ pub enum RtnlMessage {
     NewAddress(AddressMessage),
     DelAddress(AddressMessage),
     GetAddress(AddressMessage),
+    NewQueueDiscipline(TcMessage),
+    DelQueueDiscipline(TcMessage),
+    GetQueueDiscipline(TcMessage),
+    NewTrafficClass(TcMessage),
+    DelTrafficClass(TcMessage),
+    GetTrafficClass(TcMessage),
+    NewTrafficFilter(TcMessage),
+    DelTrafficFilter(TcMessage),
+    GetTrafficFilter(TcMessage),
 }
 
 impl RtnlMessage {
@@ -73,6 +83,78 @@ impl RtnlMessage {
         }
     }
 
+    pub fn is_new_qdisc(&self) -> bool {
+        if let RtnlMessage::NewQueueDiscipline(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_del_qdisc(&self) -> bool {
+        if let RtnlMessage::DelQueueDiscipline(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_get_qdisc(&self) -> bool {
+        if let RtnlMessage::GetQueueDiscipline(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_new_class(&self) -> bool {
+        if let RtnlMessage::NewTrafficClass(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_del_class(&self) -> bool {
+        if let RtnlMessage::DelTrafficClass(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_get_class(&self) -> bool {
+        if let RtnlMessage::GetTrafficClass(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_new_filter(&self) -> bool {
+        if let RtnlMessage::NewTrafficFilter(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_del_filter(&self) -> bool {
+        if let RtnlMessage::DelTrafficFilter(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn is_get_filter(&self) -> bool {
+        if let RtnlMessage::GetTrafficFilter(_) = *self {
+            true
+        } else {
+            false
+        }
+    }
+
     pub fn message_type(&self) -> u16 {
         use self::RtnlMessage::*;
 
@@ -84,9 +166,19 @@ impl RtnlMessage {
             NewAddress(_) => RTM_NEWADDR,
             DelAddress(_) => RTM_DELADDR,
             GetAddress(_) => RTM_GETADDR,
+            NewQueueDiscipline(_) => RTM_NEWQDISC,
+            DelQueueDiscipline(_) => RTM_DELQDISC,
+            GetQueueDiscipline(_) => RTM_GETQDISC,
+            NewTrafficClass(_) => RTM_NEWTCLASS,
+            DelTrafficClass(_) => RTM_DELTCLASS,
+            GetTrafficClass(_) => RTM_GETTCLASS,
+            NewTrafficFilter(_) => RTM_NEWTFILTER,
+            DelTrafficFilter(_) => RTM_DELTFILTER,
+            GetTrafficFilter(_) => RTM_GETTFILTER,
         }
     }
 
+    #[rustfmt::skip]
     pub(crate) fn parse(message_type: u16, buffer: &[u8]) -> Result<Self, DecodeError> {
         use self::RtnlMessage::*;
         let message = match message_type {
@@ -115,6 +207,30 @@ impl RtnlMessage {
                     _ => unreachable!(),
                 }
             }
+            // TC Messages
+            RTM_NEWQDISC
+            | RTM_DELQDISC
+            | RTM_GETQDISC
+            | RTM_NEWTCLASS
+            | RTM_DELTCLASS
+            | RTM_GETTCLASS
+            | RTM_NEWTFILTER
+            | RTM_DELTFILTER
+            | RTM_GETTFILTER => {
+                let msg: TcMessage = TcBuffer::new(&self.payload()).parse()?;
+                match header.message_type() {
+                    RTM_NEWQDISC => NewQueueDiscipline(msg),
+                    RTM_DELQDISC => DelQueueDiscipline(msg),
+                    RTM_GETQDISC => GetQueueDiscipline(msg),
+                    RTM_NEWTCLASS => NewTrafficClass(msg),
+                    RTM_DELTCLASS => DelTrafficClass(msg),
+                    RTM_GETTCLASS => GetTrafficClass(msg),
+                    RTM_NEWTFILTER => NewTrafficFilter(msg),
+                    RTM_DELTFILTER => DelTrafficFilter(msg),
+                    RTM_GETTFILTER => GetTrafficFilter(msg),
+                    _ => unreachable!(),
+                }
+            }
             _ => return Err(format!("Unknown message type: {}", message_type).into()),
         };
         Ok(message)
@@ -135,6 +251,17 @@ impl Emitable for RtnlMessage {
             | NewAddress(ref msg)
             | DelAddress(ref msg)
             | GetAddress(ref msg)
+            => msg.buffer_len(),
+
+            | NewQueueDiscipline(ref msg)
+            | DelQueueDiscipline(ref msg)
+            | GetQueueDiscipline(ref msg)
+            | NewTrafficClass(ref msg)
+            | DelTrafficClass(ref msg)
+            | GetTrafficClass(ref msg)
+            | NewTrafficFilter(ref msg)
+            | DelTrafficFilter(ref msg)
+            | GetTrafficFilter(ref msg)
             => msg.buffer_len()
         }
     }
@@ -152,6 +279,17 @@ impl Emitable for RtnlMessage {
             | NewAddress(ref msg)
             | DelAddress(ref msg)
             | GetAddress(ref msg)
+            => msg.emit(buffer),
+
+            | NewQueueDiscipline(ref msg)
+            | DelQueueDiscipline(ref msg)
+            | GetQueueDiscipline(ref msg)
+            | NewTrafficClass(ref msg)
+            | DelTrafficClass(ref msg)
+            | GetTrafficClass(ref msg)
+            | NewTrafficFilter(ref msg)
+            | DelTrafficFilter(ref msg)
+            | GetTrafficFilter(ref msg)
             => msg.emit(buffer)
         }
     }
